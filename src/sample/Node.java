22c85bbf6d1a2d507efe9cb7381a7d940dba77ec
kwazzi <54319675+kwazzi@users.noreply.github.com>
2019-12-03 09:50:29 -0600

    made getters and setters

--- src/sample/Node.java
+++ src/sample/Node.java
@@ -3,7 +3,9 @@ package sample;
 public class Node {
     int value; int xPos; int yPos;
     int [][] area = new int[3][3];
-    int mapTime[][];
+    int mapTime[][]; 
+    Node first; Node second; Node third; Node fourth; 
+    Node fifth; Node sixth; Node seventh; Node eight;
 
     public Node(){
 
@@ -68,4 +70,69 @@ public class Node {
     public void setArea(int[][] area) {
         this.area = area;
     }
+
+    public Node getEight() {
+        return eight;
+    }
+
+    public Node getFifth() {
+        return fifth;
+    }
+
+    public Node getFirst() {
+        return first;
+    }
+
+    public Node getFourth() {
+        return fourth;
+    }
+
+    public Node getSecond() {
+        return second;
+    }
+
+    public Node getSeventh() {
+        return seventh;
+    }
+
+    public Node getSixth() {
+        return sixth;
+    }
+
+    public Node getThird() {
+        return third;
+    }
+
+    public void setEight(Node eight) {
+        this.eight = eight;
+    }
+
+    public void setFifth(Node fifth) {
+        this.fifth = fifth;
+    }
+
+    public void setFirst(Node first) {
+        this.first = first;
+    }
+
+    public void setFourth(Node fourth) {
+        this.fourth = fourth;
+    }
+
+    public void setSecond(Node second) {
+        this.second = second;
+    }
+
+    public void setSeventh(Node seventh) {
+        this.seventh = seventh;
+    }
+
+    public void setSixth(Node sixth) {
+        this.sixth = sixth;
+    }
+
+    public void setThird(Node third) {
+        this.third = third;
+    }
 }
+
